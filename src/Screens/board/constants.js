97df6e6e0acbc030c96d96735d6a13d360d0494a
Jesus Armando Armenta Lopez <armanddoo15@hotmail.com>
2020-06-13 18:38:50 -0600

    ya esta una parte de las validaciones

--- src/Screens/board/constants.js
+++ src/Screens/board/constants.js
@@ -49,25 +49,30 @@ export const initialState = {
       name: 'abbey',
       color: 'naranja',
       description: '',
-      src: naranjaPng
+      src: naranjaPng,
+      materiales: ['vidrio','ladrillo','piedra','piedra']
     },
     {
       name: 'chapel',
       color: 'naranja',
       description: '',
-      src: naranjaPng
+      src: naranjaPng,
+      materiales: ['vidrio','piedra','vidrio','piedra']
+      
     },
     {
       name: 'cloister',
       color: 'naranja',
       description: '',
-      src: naranjaPng
+      src: naranjaPng,
+      materiales: ['vidrio','madera','ladrillo','piedra']
     },
     {
       name: 'temple',
       color: 'naranja',
       description: '',
-      src: naranjaPng
+      src: naranjaPng,
+      materiales: ['vidrio','ladrillo','ladrillo','piedra']
     },
   ],
   rojo: [
@@ -75,91 +80,107 @@ export const initialState = {
       name: 'farm',
       color: 'rojo',
       description: '',
-      src: rojaPng
+      src: rojaPng,
+      materiales: ['trigo','trigo','madera','madera']
     },
     {
       name: 'granary',
       color: 'rojo',
       description: '',
-      src: rojaPng
+      src: rojaPng,
+      materiales: ['trigo','trigo','madera','ladrillo']
     },
     {
       name: 'greenhouse',
       color: 'rojo',
       description: '',
-      src: rojaPng
+      src: rojaPng,
+      materiales: ['trigo','vidrio','madera','madera']
     },
     {
       name: 'orchard',
       color: 'rojo',
       description: '',
-      src: rojaPng
+      src: rojaPng,
+      materiales: ['piedra','trigo','trigo','madera']
     },
   ],
   verde: [
     {
       name: 'almshouse',
       color: 'verde',
-      src: verdePng
+      src: verdePng,
+      materiales: ['piedra','piedra','vidrio']
     },
     {
       name: 'feast hall',
       color: 'verde',
-      src: verdePng
+      src: verdePng,
+      materiales: ['madera','madera','vidrio']
     },
     {
       name: 'inn',
       color: 'verde',
-      src: verdePng
+      src: verdePng,
+      materiales: ['trigo','piedra','vidrio']
     },
     {
       name: 'tavern',
       color: 'verde',
-      src: verdePng
+      src: verdePng,
+      materiales: ['ladrillo','ladrillo','vidrio']
     },
   ],
   gris: [
     {
       name: 'fountain',
       color: 'gris',
-      src: grisPng
+      src: grisPng,
+      materiales: ['madera','piedra']
     },
     {
       name: 'millstone',
       color: 'gris',
-      src: grisPng
+      src: grisPng,
+      materiales: ['madera','piedra']
     },
     {
       name: 'shed',
       color: 'gris',
-      src: grisPng
+      src: grisPng,
+      materiales: ['madera','piedra']
     },
     {
       name: 'well',
       color: 'gris',
-      src: grisPng
+      src: grisPng,
+      materiales: ['madera','piedra']
     },
   ],
   amarillo: [
     {
       name: 'bakery',
       color: 'amarillo',
-      src: amarilloPng
+      src: amarilloPng,
+      materiales: ['trigo','ladrillo','vidrio','ladrillo']
     },
     {
       name: 'market',
       color: 'amarillo',
-      src: amarilloPng
+      src: amarilloPng,
+      materiales: ['madera','piedra','vidrio','piedra']
     },
     {
       color: 'amarillo',
       name: 'tailor',
-      src: amarilloPng
+      src: amarilloPng,
+      materiales: ['trigo','piedra','vidrio','piedra']
     },
     {
       name: 'theater',
       color: 'amarillo',
-      src: amarilloPng
+      src: amarilloPng,
+      materiales: ['piedra','madera','vidrio','madera']
     },
   ],
   azul: [
@@ -167,72 +188,88 @@ export const initialState = {
       name: 'bank',
       color: 'azul',
       src: negraPng,
+      materiales: ['trigo','trigo','madera','vidrio','ladrillo']
     },
     {
       name: 'factory',
       color: 'azul',
       src: negraPng,
+      materiales: ['madera','ladrillo','piedra','piedra','ladrillo']
     },
     {
       name: 'trading post',
       color: 'azul',
       src: negraPng,
+      materiales: ['piedra','madera','piedra','madera','ladrillo']
     },
 
     {
       name: 'warehouse',
       color: 'azul',
       src: negraPng,
+      materiales: ['trigo','madera','trigo','ladrillo','ladrillo']
     },
   ],
   rosa: [
     {
       name: 'architect´s guild',
       color: 'rosa',
+      materiales: ['vidrio','trigo','piedra','madera','ladrillo']
     },
     {
       name: 'archive of the second age',
       color: 'rosa',
+      materiales: ['trigo','trigo','ladrillo','vidrio']
     },
     {
       name: 'barrett castle',
       color: 'rosa',
+      materiales: ['trigo','piedra','madera','vidrio','vidrio','ladrillo']
     },
     {
       name: 'cathedral of caterina',
       color: 'rosa',
+      materiales: ['trigo','piedra','vidrio']
     },
     {
       name: 'fort ironweed',
       color: 'rosa',
+      materiales: ['trigo','ladrillo','piedra','madera','piedra']
     },
     {
       name: 'grove university',
       color: 'rosa',
+      materiales: ['ladrillo','piedra','vidrio','piedra']
     },
     {
       name: 'mandras palace',
       color: 'rosa',
+      materiales: ['trigo','vidrio','ladrillo','madera']
     },
     {
       name: 'opaleye´s watch',
       color: 'rosa',
+      materiales: ['madera','ladrillo','vidrio','trigo','trigo','piedra']
     },
     {
-      name: 'shirne of the elder tree',
+      name: 'shrine of the elder tree',
       color: 'rosa',
+      materiales: ['ladrillo','trigo','piedra','madera','vidrio','madera']
     },
     {
       name: 'silva forum',
       color: 'rosa',
+      materiales: ['trigo','ladrillo','ladrillo','piedra','madera']
     },
     {
       name: 'the starloom',
       color: 'rosa',
+      materiales: ['vidrio','vidrio','madera','trigo']
     },
     {
       name: 'statue of the bondmaker',
       color: 'rosa',
+      materiales: ['madera','piedra','piedra','vidrio','trigo']
     },
   ],
   tablero: [
